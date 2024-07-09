41649d1a25b639e8ca2cb3bf6bc2d3d21e29e8f6
Loor Sawalhi <loorwael@yahoo.com>
2024-07-09 16:21:52 +0300

    Edited branch title

--- Program.fs
+++ Program.fs
@@ -2,7 +2,7 @@
 open System.Collections.Generic
 
 
-(*6.Numbers bigger than 1000 should be ignored, so adding 2 + 1001  = 2*)
+(*7.Delimiters can be of any length with the following format:  “//[delimiter]\n” for example: “//[***]\n1***2***3” should return 6*)
 
 exception NegativeNumber of string
 let ConvertStringToInteger ( number : string) =
