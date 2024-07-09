e216460d5d4a3bd3dd7b8d6b4d077f8a1bc4272c
Loor Sawalhi <loorwael@yahoo.com>
2024-07-09 22:13:05 +0300

    Fixed basic addition

--- Program.fs
+++ Program.fs
@@ -19,38 +19,27 @@ let Add( numbers : string )( delimiter : char) =
     if (numbers.Length <= 0) then
         Some(0)
     else
-        
-        let numbersArray = numbers.Split @$"{delimiter}"
-        
-        let mutable result : int = 0
-       
         try
+            if numbers.Contains(@",\n") || numbers.Contains(@"\n,") then
+                raise (FormatException())
+            
+            let numbersArray = numbers.Replace(@"\n",",").Split @$"{delimiter}"
+            
+            let mutable sum : int = 0
+       
+        
             let negativeNums = new List<string>()
             for num in numbersArray do
-                let splitByComma = num.Split ","
-                let stringLength = num.Length - 1
-                let mutable sum : int = 0
-                let innerValues =
-                    if (num.Length <> 0 && (num[0] = ',' || num[stringLength] = ',')) then
-                        None
-                    else
-                        for i in splitByComma do
-                            let x = ConvertStringToInteger i
-                            if (x < 0) then
-                                negativeNums.Add(i)
-                            sum <- x + sum
-                        Some(sum)
-                
-                result <-
-                    match innerValues with
-                    | None -> raise (FormatException("Wrong Formating"))
-                    | _ -> result + innerValues.Value
+                let x = ConvertStringToInteger num
+                if (x < 0) then
+                    negativeNums.Add(num)
+                sum <- x + sum
                     
 
             if (negativeNums.Count <> 0) then
                 let fullString = String.concat ", " negativeNums
                 raise (NegativeNumber(fullString))
-            Some(result)        
+            Some(sum)        
         with
             | :? FormatException -> None
             | NegativeNumber(e)-> printfn $"Negatives are not allowed : {e}";  None
@@ -60,9 +49,16 @@ let mutable condition = true
 while condition do
     printf @"Enter your string of numbers, separate by new lines \n :"
     let numbersString = Console.ReadLine()
-    let index = numbersString.IndexOf @"\n"
-    let delimiter = numbersString[2]
-    let numbers = numbersString[index..].Replace(@"\n", delimiter.ToString()).Replace(',', delimiter)
+    let mutable delimiter = ','
+    let mutable numbers = numbersString
+        
+    if numbersString.Contains("//") then
+        let index = numbersString.IndexOf @"\n"
+        delimiter <- numbersString[2]
+        numbers <- numbersString[index..].Replace(@"\n", delimiter.ToString()).Replace(',', delimiter)
+    else
+        numbers <- numbersString
+        delimiter <- ','
     
     printfn $"Your delimiter is {delimiter}"
     
