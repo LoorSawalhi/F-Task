9dfa41998ede4986ef54481fbb7787339c1138fa
Loor Sawalhi <loorwael@yahoo.com>
2024-07-09 16:16:03 +0300

    Added customization for multiple character separators

--- Program.fs
+++ Program.fs
@@ -13,39 +13,24 @@ let ConvertStringToInteger ( number : string) =
         let x : int = int number
         x
       
-let Add( numbers : string )( delimiter : char) =
+let Add( numbers : string )( delimiter : string) =
     
     if (numbers.Length <= 0) then
         Some(0)
     else
         
         let numbersArray = numbers.Split @$"{delimiter}"
-        
-        let mutable result : int = 0
-       
+        let mutable sum : int = 0
+
         try
             for num in numbersArray do
                 let splitByComma = num.Split ","
                 let stringLength = num.Length - 1
-                let mutable sum : int = 0
-                let innerValues =
-                    if (num.Length <> 0 && (num[0] = ',' || num[stringLength] = ',')) then
-                        None
-                    else
-                        for i in splitByComma do
-                            let x = ConvertStringToInteger i
-                            
-                            if (x < 1000) then
-                                sum <- x + sum
-                        Some(sum)
-                
-                result <-
-                    match innerValues with
-                    | None -> raise (FormatException("Wrong Formating"))
-                    | _ -> result + innerValues.Value
-                    
+                for i in splitByComma do
+                    let x = ConvertStringToInteger i
+                    sum <- x + sum
 
-            Some(result)        
+            Some(sum)        
         with
             | :? FormatException -> None
 
@@ -55,7 +40,7 @@ while condition do
     printf @"Enter your string of numbers, separate by new lines \n :"
     let numbersString = Console.ReadLine()
     let inputs =  numbersString.Split @"\n"
-    let delimiter = inputs[0][2]
+    let delimiter = inputs[0].Trim('/').Trim('[').Trim(']')
     
     printfn $"Your delimiter is {delimiter}"
     
@@ -71,4 +56,3 @@ while condition do
     
     if exitCondition = "1" then
         condition <- false
-    
