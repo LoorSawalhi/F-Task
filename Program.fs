8ce5ad99d6896045182cea1a478e1902f26e720b
Loor Sawalhi <loorwael@yahoo.com>
2024-07-09 10:03:15 +0300

    Added logic for simple calculator

--- Program.fs
+++ Program.fs
@@ -1,2 +1,49 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let ConvertStringToInteger ( number : string) =
+    let tremmedNumber = number.Trim()
+    if(tremmedNumber.Length <= 0) then
+        0
+    else
+        let x : int = int number
+        x
+      
+let Add( numbers : string ) =
+    
+    if (numbers.Length <= 0) then
+        Some(0)
+    else
+        
+        let numbersArray = numbers.Split ","
+        let mutable sum : int = 0
+        
+        if (numbersArray.Length > 3) then
+           None
+        else
+            try
+                for num in numbersArray do
+                    let x = ConvertStringToInteger num
+                    sum <- x + sum
+                Some(sum)
+            with
+                | :? FormatException ->  None
+        
+
+let mutable condition = true
+
+while condition do
+    printf @"Enter your string of numbers, separate by comma :"
+    let numbersString = Console.ReadLine()
+    let answer = Add numbersString
+    
+    match answer with
+    | None -> printfn $"Wrong Format"
+    | _ -> printfn $"Answer is %d{answer.Value}"
+    
+    printf "Wants to exit? press 1 is yes : "
+    
+    let exitCondition = Console.ReadLine()
+    
+    if exitCondition = "1" then
+        condition <- false
+    
