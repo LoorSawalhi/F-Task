1a4576cb1693e6b3491e0b009d7b17fe6250d4a3
Loor Sawalhi <loorwael@yahoo.com>
2024-07-15 12:09:43 +0300

    Working with happy path

--- Program.fs
+++ Program.fs
@@ -1,13 +1,13 @@
 ﻿open System
 
-exception NegativeNumber of string
+exception NegativeNumber of int array
 let convertStringToInteger ( number : string) =
     match number.Trim() with
     | "" -> Some(0) 
     | trimmed ->
         match Int32.TryParse(trimmed) with
         | true, parsed -> Some(parsed)  
-        | _ -> None  
+        | _ -> raise(FormatException())  
 
 let delimiterAndNumbersExtractor (numbersString: string) =
     let cleanedDelimiters, numbers =
@@ -25,56 +25,33 @@ let delimiterAndNumbersExtractor (numbersString: string) =
             
     cleanedDelimiters, numbers
     
-let filterNegatives (numbers : int option array) =
+let filterNegatives (numbers : int array) =
     let negatives =
         numbers
-        |> Array.filter (fun n -> int n.Value < 0)
-        |> Array.map(string) 
-        |> String.concat ", "
-        
-    let hasNegatives =
-        match negatives with
-        | "" -> (false, "")
-        | _ -> (true, negatives)
-    hasNegatives
-    
-let processNumbers (numbers : int option array) =
-
-    match Array.TrueForAll (numbers, _.IsSome) with
-    | true ->
-        numbers
-        |> filterNegatives
-        |> fun (hasNegatives, negativeList) ->
-           try
-               match hasNegatives with
-               | true -> raise (NegativeNumber(negativeList))
-               | false -> Some(Array.sum <| Array.choose id numbers)
-           with
-           | NegativeNumber(e)-> printfn $"Negatives are not allowed : {e}";  None
-    | false ->
-        None
+        |> Array.filter (fun n -> int n < 0)
+    match negatives.Length with
+    | 0 -> numbers
+    | _ -> raise(NegativeNumber(negatives))
 
 let add( numbersString : string ) =
     
     let delimiters, numbers = delimiterAndNumbersExtractor numbersString
-    let result =
-        match numbers with
-        | _ when numbers.Contains(@",\n") || numbers.Contains(@"\n,") ->
-            None
-        | _ -> numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
-                |> Array.map(convertStringToInteger)
-                |> Array.filter (fun n ->
-                    match n with
-                    | None -> true
-                    | Some(v) -> v <= 1000)
-                |> processNumbers 
-   
-    result
-  
+
+    numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map(convertStringToInteger)
+    |> Array.choose id
+    |> Array.filter (fun v -> v <= 1000)
+    |> filterNegatives
+    |> Array.sum
+    
+     
 printf @"Enter your string of numbers :"
 let numbersString = Console.ReadLine()
-let answer = add numbersString
 
-match answer with
-| None -> printfn "Wrong Format"
-| Some i -> printfn $"Answer is %d{i}"
+try
+    let answer = add numbersString
+
+    printfn $"Answer is %d{answer}"
+with
+ | NegativeNumber(e)-> printfn $"Negatives are not allowed : {e}"
+ | _ -> printfn "Wrong Format"
